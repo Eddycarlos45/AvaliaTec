28282372dc951210d2f2ab243d7643c627d8b31c
Eddycarlos45 <edsoncarlos33@hotmail.com>
2020-03-31 17:14:37 -0300

    Added getUsers

--- avaliatec-functions/functions/index.js
+++ avaliatec-functions/functions/index.js
@@ -1,16 +1,17 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
-const { signup, login } = require('./user/userActivity');
+const { signup, login, getUsers } = require('./user/userActivity');
 const { createTheme, getThemes } = require('./theme/themes');
-const { createForm, getForms } = require('./form/forms')
+const { createForm, getForms } = require('./form/forms');
 
 //Users route
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/theme', createTheme);
-app.get('/theme', getThemes);
 app.post('/form', createForm);
 app.get('/form', getForms);
+app.get('/theme', getThemes);
+app.get('/users', getUsers);
 
 exports.api = functions.https.onRequest(app); 
