5e425d455edf9d92180fc99fb7e66f337d3f29d7
Filipe Dias Gianotto <8786327+filipedg@users.noreply.github.com>
2020-05-28 20:46:42 -0300

    Return available forms

--- avaliatec-native/components/login.component.js
+++ avaliatec-native/components/login.component.js
@@ -1,14 +1,94 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native'
 import { TextInput, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { AsyncStorage } from 'react-native';
+import { StackActions } from '@react-navigation/native';
 import styles from '../assets/stylesheet/Custom'
+import axios from 'axios';
+
+const proxy = 'https://us-central1-avaliatec-80c1a.cloudfunctions.net/api/login'
 
 export const LoginScreen = ({ navigation }) => {
 
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const [authEmail, setAuthEmail] = React.useState('');
+    const [authPassword, setAuthPassword] = React.useState('');
+    const [authStatus, setAuthStatus] = React.useState('false');
+    const [firstRun, setFirstRun] = React.useState('true');
 
-    const { signIn } = React.useContext(AuthContext);
+    // CHECK AUTHENTICATION
+    handleAuthentication = async () => {
+        try {
+            if (await AsyncStorage.getItem('USER') != null) {
+                // AUTHENTICATION DETECTED
+                console.log('AUTHENTICATION DETECTED, REDIRECTING...')
+                setAuthStatus('true')
+                // navigation.dispatch(
+                //     StackActions.replace('Home')
+                // );
+                // navigation.push('Home')
+            } else {
+                // AUTHENTICATION NOT DETECTED
+                console.log('AUTHENTICATION NOT DETECTED, THERE\'S NOTHING TO DO...')
+                setAuthStatus('false')
+                // navigation.dispatch(
+                //     StackActions.replace('Login')
+                // );
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    // SAVE AUTHENTICATION
+    saveAuthentication = async () => {
+        try {
+            await AsyncStorage.setItem('USER', authEmail)
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    // const signIn = async () => {
+    //     try {
+    //         await AsyncStorage.setItem('USER', authEmail);
+    //         handleAuthentication()
+    //     } catch (error) {
+    //         console.log(error.message);
+    //     }
+    // };
+
+    handleSubmit = (event) => {
+        // event.preventDefault();
+        // this.setState({
+        //     loading: true
+        // });
+        const userData = {
+            email: authEmail,
+            password: authPassword
+        }
+
+        axios.post(proxy, userData)
+            .then(res => {
+                console.log('LOGIN SUCCESS')
+                saveAuthentication()
+                handleAuthentication()
+            })
+            .catch(err => {
+                // this.setState({
+                //     errors: err.response.data,
+                //     loading: false
+                // })
+                console.log('LOGIN FAIL')
+            })
+    }
+
+    // // ON APP START
+    // (async function () {
+    //     if (firstRun == 'true') {
+    //         setFirstRun('false')
+    //         handleAuthentication()
+    //     }
+    // })();
 
     return (
         <View style={styles.middle}>
@@ -19,13 +99,13 @@ export const LoginScreen = ({ navigation }) => {
                         <TextInput
                             style={styles.marginBottom}
                             label='E-mail'
-                            value={email}
-                            onChangeText={value => setEmail(value)}
+                            value={authEmail}
+                            onChangeText={value => setAuthEmail(value)}
                         />
                         <TextInput
                             label='Password'
-                            value={password}
-                            onChangeText={value => setPassword(value)}
+                            value={authPassword}
+                            onChangeText={value => setAuthPassword(value)}
                         />
                     </Card.Content>
                     <Card.Actions
@@ -34,12 +114,12 @@ export const LoginScreen = ({ navigation }) => {
                         <Button
                             style={styles.marginRight}
                             mode="text"
-                            onPress={() => console.log(email)}>
+                            onPress={() => console.log('NOT IMPLEMENTED')}>
                             Esquecí a senha
                         </Button>
                         <Button
                             mode="contained"
-                            onPress={() => signIn({ username, password })}>
+                            onPress={() => handleSubmit()}>
                             Acessar
                         </Button>
                     </Card.Actions>
