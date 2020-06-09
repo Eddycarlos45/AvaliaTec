fd27928e33174a4ad856b47d43bb730d4e78d80f
Filipe Dias Gianotto <8786327+filipedg@users.noreply.github.com>
2020-06-08 21:17:31 -0300

    Revert "Revert "Login screen completed""
    This reverts commit d676f52f7ed69ab092165e3bc72604a13b9b4be5.

--- avaliatec-native/components/login.component.js
+++ avaliatec-native/components/login.component.js
@@ -48,20 +48,7 @@ export const LoginScreen = ({ navigation }) => {
         }
     }
 
-    // const signIn = async () => {
-    //     try {
-    //         await AsyncStorage.setItem('USER', authEmail);
-    //         handleAuthentication()
-    //     } catch (error) {
-    //         console.log(error.message);
-    //     }
-    // };
-
     handleSubmit = (event) => {
-        // event.preventDefault();
-        // this.setState({
-        //     loading: true
-        // });
         const userData = {
             email: authEmail,
             password: authPassword
@@ -74,21 +61,9 @@ export const LoginScreen = ({ navigation }) => {
                 handleAuthentication()
             })
             .catch(err => {
-                // this.setState({
-                //     errors: err.response.data,
-                //     loading: false
-                // })
                 console.log('LOGIN FAIL')
             })
     }
-
-    // // ON APP START
-    // (async function () {
-    //     if (firstRun == 'true') {
-    //         setFirstRun('false')
-    //         handleAuthentication()
-    //     }
-    // })();
 
     return (
         <View style={styles.middle}>
@@ -98,12 +73,14 @@ export const LoginScreen = ({ navigation }) => {
                         <Title>Acesso</Title>
                         <TextInput
                             style={styles.marginBottom}
+                            secureTextEntry={false}
                             label='E-mail'
                             value={authEmail}
                             onChangeText={value => setAuthEmail(value)}
                         />
                         <TextInput
                             label='Password'
+                            secureTextEntry={true}
                             value={authPassword}
                             onChangeText={value => setAuthPassword(value)}
                         />
