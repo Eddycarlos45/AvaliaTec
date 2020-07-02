b031ef0085f0e20818922a5989017f1f5cce0440
Filipe Dias Gianotto <8786327+filipedg@users.noreply.github.com>
2020-07-01 22:31:11 -0300

    Sign in screen updated
    This update brings the hability to request a password reset email.

--- avaliatec-native/components/login.component.test.js
+++ avaliatec-native/components/login.component.test.js
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import { View, ScrollView, Text } from 'react-native'
-import { TextInput, Button, Card, Divider } from 'react-native-paper';
+import { TextInput, Button, Card, Divider, FAB } from 'react-native-paper';
 import { AsyncStorage } from 'react-native';
 import styles from '../assets/stylesheet/General'
 import axios from 'axios';
 
 const axiosSignIn = 'https://us-central1-avaliatec-80c1a.cloudfunctions.net/api/login'
 const axiosGetUsers = 'https://us-central1-avaliatec-80c1a.cloudfunctions.net/api/users'
+const axiosRequestNewPassword = 'https://us-central1-avaliatec-80c1a.cloudfunctions.net/api/resetPassword'
 
 const strings = {
-    title: 'Avaliatec',
-    email: 'E-mail',
-    password: 'Senha',
-    signIn: 'Acessar',
-    signingIn: 'Acessando',
-    fail: 'E-mail ou senha incorretos'
+    appName: 'Avaliatec',
+    signInTitle: 'Entre na sua conta',
+    signInEmail: 'E-mail',
+    signInPassword: 'Senha',
+    signIn: 'Entrar',
+    signingIn: 'Entrando',
+    signInFail: 'E-mail ou senha incorretos',
+    requestNewPasswordTitle: 'Solicite uma nova senha',
+    requestNewPasswordEmail: 'E-mail da sua conta',
+    requestNewPassword: 'Solicitar',
+    requestingNewPassword: 'Solicitando',
+    requestNewPasswordSuccess: 'Instruções encaminhadas',
+    requestNewPasswordFail: 'Nenhuma conta encontrada',
 }
 
 export default class LoginScreen extends React.Component {
@@ -22,13 +30,17 @@ export default class LoginScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: null,
-            password: null,
-            authStatus: false,
-            firstRun: true,
-            loading: false,
-            fail: false,
+            signInEmail: null,
+            signInPassword: null,
+            signInStatus: null,
+            requestNewPasswordEmail: null,
+            requestNewPasswordStatus: -1,
+            signInLoading: false,
+            requestNewPasswordLoading: false,
             users: [],
+            firstRun: true,
+            authStatus: false,
+            screen: true
         };
     }
 
@@ -38,8 +50,8 @@ export default class LoginScreen extends React.Component {
             axios.get(axiosGetUsers)
                 .then(res => {
                     res.data.map((user) => {
-                        if (user.email == this.state.email) {
-                            AsyncStorage.setItem('EMAIL', this.state.email)
+                        if (user.email == this.state.signInEmail) {
+                            AsyncStorage.setItem('EMAIL', this.state.signInEmail)
                             AsyncStorage.setItem('NAME', user.userName)
                             console.log(AsyncStorage.getItem('EMAIL', (err, item) => item))
                             console.log(AsyncStorage.getItem('NAME', err))
@@ -56,69 +68,141 @@ export default class LoginScreen extends React.Component {
         }
     }
 
-    _handleSignIn(event) {
+    _handleSignIn() {
         const userData = {
-            email: this.state.email,
-            password: this.state.password
+            email: this.state.signInEmail,
+            password: this.state.signInPassword
         }
 
-        this.setState({ loading: true })
+        this.setState({ signInLoading: true })
 
         axios.post(axiosSignIn, userData)
             .then(res => {
                 this._storeSignInData()
-                this.setState({ loading: false })
+                this.setState({ signInLoading: false })
                 console.log('LOGIN SUCCESS')
             })
             .catch(err => {
-                this.setState({ loading: false, fail: true })
+                this.setState({ signInLoading: false, singInStatus: true })
                 console.log('LOGIN FAIL')
             })
     }
 
+    _handleRequestNewPassword() {
+        const userReset = {
+            email: this.state.requestNewPasswordEmail,
+        }
+
+        this.setState({ requestNewPasswordLoading: true })
+
+        axios.post(axiosRequestNewPassword, userReset)
+            .then(res => {
+                this._storeSignInData()
+                this.setState({ requestNewPasswordLoading: false, requestNewPasswordStatus: 1 })
+                console.log('PASSWORD REQUEST SUCCESS')
+            })
+            .catch(err => {
+                this.setState({ requestNewPasswordLoading: false, requestNewPasswordStatus: 0 })
+                console.log('PASSWORD REQUEST FAIL')
+            })
+    }
+
+    _switchScreen() {
+        this.setState({ screen: !this.state.screen })
+    }
+
     render() {
+        if (this.state.screen) { }
         return (
-            <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }} >
                 <ScrollView style={[styles.container, styles.containerCenter]} contentContainerStyle={[styles.screen, styles.screenCenter]}>
-                    <Card style={[styles.card, styles.spacer]}>
-                        <Card.Title titleStyle={styles.title} title={strings.title} />
-                        <Divider />
-                        <Card.Content style={[styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
-                            <TextInput
-                                style={[styles.spacer, styles.inputText]}
-                                mode='outlined'
-                                secureTextEntry={false}
-                                label={strings.email}
-                                value={this.state.email}
-                                error={this.state.email == '' ? true : false}
-                                onChangeText={value => this.setState({ email: value })}
-                            />
-                            <TextInput
-                                style={[styles.inputText]}
-                                label={strings.password}
-                                mode='outlined'
-                                secureTextEntry={true}
-                                value={this.state.password}
-                                error={this.state.password == '' ? true : false}
-                                onChangeText={value => this.setState({ password: value })}
-                            />
-                        </Card.Content>
-                        <Divider />
-                        <Card.Actions style={[styles.cardActionsRight, styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
-                            <Text style={styles.errorText}>{this.state.fail ? strings.fail : ''}</Text>
-                            <Button
-                                disabled={this.state.loading ? true : false}
-                                mode='contained'
-                                onPress={() =>
-                                    this._handleSignIn()
-                                }
-                                loading={this.state.loading ? true : false}
-                            >
-                                {this.state.loading ? strings.signingIn : strings.signIn}
-                            </Button>
-                        </Card.Actions>
-                    </Card>
+                    {
+                        !this.state.screen ?
+                            (
+                                <Card style={[styles.card, styles.spacer]}>
+                                    <Card.Title style={styles.defaultMarginVertical} titleStyle={styles.title} title={strings.requestNewPasswordTitle} />
+                                    <Divider />
+                                    <Card.Content style={[styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
+                                        <TextInput
+                                            style={[styles.spacer, styles.inputText]}
+                                            mode='outlined'
+                                            secureTextEntry={false}
+                                            label={strings.requestNewPasswordEmail}
+                                            value={this.state.requestNewPasswordEmail}
+                                            error={this.state.requestNewPasswordEmail == '' ? true : false}
+                                            onChangeText={value => this.setState({ requestNewPasswordEmail: value })}
+                                        />
+                                    </Card.Content>
+                                    <Divider />
+                                    <Card.Actions style={[styles.cardActionsRight, styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
+                                        <Text
+                                            style={this.state.requestNewPasswordStatus == 0 ? styles.errorText : this.state.requestNewPasswordStatus == 1 ? styles.successText : styles.normalText}
+                                        >
+                                            {this.state.requestNewPasswordStatus == 0 ? strings.requestNewPasswordFail : this.state.requestNewPasswordStatus == 1 ? strings.requestNewPasswordSuccess : ''}
+                                        </Text>
+                                        <Button
+                                            disabled={this.state.requestNewPasswordLoading ? true : false}
+                                            mode='contained'
+                                            onPress={() =>
+                                                this._handleRequestNewPassword()
+                                            }
+                                            loading={this.state.requestNewPasswordLoading ? true : false}
+                                        >
+                                            {this.state.requestNewPasswordLoading ? strings.requestingNewPassword : strings.requestNewPassword}
+                                        </Button>
+                                    </Card.Actions>
+                                </Card>
+                            )
+                            :
+                            (
+                                <Card style={[styles.card, styles.spacer]}>
+                                    <Card.Title style={styles.defaultMarginVertical} titleStyle={styles.title} title={strings.signInTitle} />
+                                    <Divider />
+                                    <Card.Content style={[styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
+                                        <TextInput
+                                            style={[styles.spacer, styles.inputText]}
+                                            mode='outlined'
+                                            secureTextEntry={false}
+                                            label={strings.signInEmail}
+                                            value={this.state.signInEmail}
+                                            error={this.state.signInEmail == '' ? true : false}
+                                            onChangeText={value => this.setState({ signInEmail: value })}
+                                        />
+                                        <TextInput
+                                            style={[styles.inputText]}
+                                            label={strings.signInPassword}
+                                            mode='outlined'
+                                            secureTextEntry={true}
+                                            value={this.state.signInPassword}
+                                            error={this.state.signInPassword == '' ? true : false}
+                                            onChangeText={value => this.setState({ signInPassword: value })}
+                                        />
+                                    </Card.Content>
+                                    <Divider />
+                                    <Card.Actions style={[styles.cardActionsRight, styles.defaultPaddingHorizontal, styles.defaultPaddingVertical]}>
+                                        <Text style={styles.errorText}>{this.state.singInStatus ? strings.signInFail : ''}</Text>
+                                        <Button
+                                            disabled={this.state.signInLoading ? true : false}
+                                            mode='contained'
+                                            onPress={() =>
+                                                this._handleSignIn()
+                                            }
+                                            loading={this.state.signInLoading ? true : false}
+                                        >
+                                            {this.state.signInLoading ? strings.signingIn : strings.signIn}
+                                        </Button>
+                                    </Card.Actions>
+                                </Card>
+                            )
+                    }
                 </ScrollView>
+                <FAB
+                    style={styles.fab}
+                    disabled={this.state.signInLoading || this.state.requestNewPasswordLoading ? true : false}
+                    icon={this.state.screen ? 'lock' : 'send'}
+                    onPress={() => this._switchScreen()}
+                    label={this.state.screen ? strings.requestNewPasswordTitle : strings.signInTitle}
+                />
             </View >
 
         )
